57bc1315531089fa333b285c4d84e08c81045df4
Vitaliy Zarubin <v.zarubin@omp.ru>
2023-11-07 23:01:09 +0300

    Start sign

--- scripts/psdk_sign.sh
+++ scripts/psdk_sign.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Sign successfully!";
+## Check psdk
+if [ -z "$PSDK_DIR" ]; then
+    echo "Not found PSDK_DIR. See more: https://developer.auroraos.ru/doc/software_development/psdk";
+    exit 1;
+fi
+
+## Get list rmp
+RPMS=$(ls $PWD | grep -i rpm | tr '\n' ';');
+
+## List to array
+IFS=';' read -r -a array <<< "$RPMS"
+
+## Show array
+echo "Files were found:";
+echo
+for line in "${array[@]}"
+do
+  echo "-> $line"
+done
+echo
+
+## Next
+echo "Sign successfully!"
